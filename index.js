c01e1bcddb4ab63fe16a902101101e0d0a088bc4
Re-zan <zannat881all@gmail.com>
2023-06-10 11:52:26 +0600

    parent review data get from local side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
@@ -8,11 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 //mongodb start
-//re-zanSchool
-//EDdTw0I7exruoSZM
-
-const uri =
-  "mongodb+srv://re-zanSchool:EDdTw0I7exruoSZM@cluster0.6wlkevy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERPASSWORD}@cluster0.6wlkevy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,13 +25,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    //project start here
+
+    const parentReviewCollention = client
+      .db("re-zanSchoolDB")
+      .collection("parents_reviews");
+
+    //parent reviews data get
+    app.get("/parentReviews", async (req, res) => {
+      const result = await parentReviewCollention.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
