d83a07ce095ccc4fd61f60827ae32e335b2c4da9
Re-zan <zannat881all@gmail.com>
2023-06-10 11:37:01 +0600

    mongodb connetion is done now

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+
+//mongodb start
+//re-zanSchool
+//EDdTw0I7exruoSZM
+
+const uri =
+  "mongodb+srv://re-zanSchool:EDdTw0I7exruoSZM@cluster0.6wlkevy.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 //testing
 app.get("/", (req, res) => {
